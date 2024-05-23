e5e5fb4f87dcceaeb63f7412d238d6c261f42adf
Aftab Alam <aftab0103.be21@chitkara.edu.in>
2024-05-23 14:11:34 +0530

    initial commit

---
+++ backend/config/ConnectDb.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose') ;
+require('dotenv').config() ; 
+const DB_URL = process.env.DB_URL ;
+
+const dbConnect = async()=>{
+    
+        await mongoose.connect(DB_URL , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          }).then(()=>{ console.log('Successfully connected to database')})
+            .catch((e)=>{console.log(e.message)}) ;
+}
+
+
+module.exports = dbConnect
